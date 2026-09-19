5266213968e597237ba76b0ba84918c8b05c0d18
Astrazione <zhmenko03@gmail.com>
2026-09-19 18:19:08 +0500

    Добавлена табличная функция (задание 2)

---
+++ GetClientPaymentsFunc.sql
@@ -0,0 +1,34 @@
+﻿CREATE OR ALTER FUNCTION dbo.GetClientPayments
+(
+	@ClientId BIGINT,
+	@Sd DATE,
+	@Ed DATE
+)
+RETURNS TABLE
+AS
+RETURN
+(
+	WITH Dates AS
+	(
+		SELECT DATEADD(DAY, n.DayNumber, @Sd) AS Dt
+		FROM (
+			SELECT TOP (
+				CASE WHEN @Ed >= @Sd 
+					THEN DATEDIFF(DAY, @Sd, @Ed) + 1
+					ELSE 0
+				END
+			)
+			ROW_NUMBER() OVER(ORDER BY (SELECT NULL)) - 1 AS DayNumber
+			FROM sys.all_objects a
+			CROSS JOIN sys.all_objects b
+		) n
+	)
+	SELECT d.Dt, ISNULL(SUM(p.Amount), 0) AS Amount
+	FROM Dates d
+	LEFT JOIN dbo.ClientPayments p
+		ON p.Client = @ClientId
+		AND p.Dt >= d.Dt
+		AND p.Dt < DATEADD(DAY, 1, d.Dt)
+	GROUP BY d.Dt
+);
+
---
+++ Task2InitAndTest.sql
@@ -0,0 +1,24 @@
+﻿-- INIT
+DROP TABLE IF EXISTS dbo.ClientPayments;
+
+CREATE TABLE dbo.ClientPayments
+(
+	Id BIGINT PRIMARY KEY, 	-- id платежа
+	Client BIGINT, 			-- id клиента
+	Dt DATETIME2(0),		-- дата платежа
+	Amount MONEY			-- сумма платежа
+);
+
+INSERT INTO dbo.ClientPayments (Id, Client, Dt, Amount) VALUES
+(1, 1, '2022-01-03T17:24:00', 100),
+(2, 1, '2022-01-05T17:24:14', 200),
+(3, 1, '2022-01-05T18:23:34', 250),
+(4, 1, '2022-01-07T10:12:38', 50),
+(5, 2, '2022-01-05T17:24:14', 278),
+(6, 2, '2022-01-10T12:39:29', 300);
+
+
+-- TEST
+SELECT * FROM dbo.GetClientPayments(1, '2022-01-02', '2022-01-07');
+SELECT * FROM dbo.GetClientPayments(2, '2022-01-04', '2022-01-11');
+
